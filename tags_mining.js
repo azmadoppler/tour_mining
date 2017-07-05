a3a53ba940ba8f14923153fdb8b8dc0677e04b1a
Thanawit Gerdprasert <thanawit.g@ku.th>
2017-07-05 11:26:52 +0900

    added tags edit some stuff

--- tags_mining.js
+++ tags_mining.js
@@ -5,12 +5,7 @@ var config = require('./config');
 var fs = require('fs');
 var geoUser = [];
 
-var T = new Twit({
-  consumer_key: 'qcUO2JG4GVMHjBKwO8dPHSaEk',
-  consumer_secret: '2phK6LFhIdQ1CVUxkGuymJl4jJv0L2ML7Wi1ZcGBcEURr2cCmB',
-  access_token: '183192102-8XCt3EIBATFUtIcszr89juvbrPxwsvGMTzqEmIC5',
-  access_token_secret: 'bUZdOIwndhk7xDVlFluqAnfVFOahoYmndG6qFmkZtouSW'
-});//insert your config here
+var T = new Twit(config);//insert your config here
 
 var currentID ;
 var searchParameter = 'ラーメン おいし';

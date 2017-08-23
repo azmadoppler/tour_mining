da00e158e890281be756a2c5f7ca50dbbc11f773
Thanawit Gerdprasert <thanawit.g@ku.th>
2017-08-23 16:11:45 +0900

    make config official

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'qcUO2JG4GVMHjBKwO8dPHSaEk'
+  , consumer_secret: '2phK6LFhIdQ1CVUxkGuymJl4jJv0L2ML7Wi1ZcGBcEURr2cCmB'
+  , access_token: '183192102-8XCt3EIBATFUtIcszr89juvbrPxwsvGMTzqEmIC5'
+  , access_token_secret: 'bUZdOIwndhk7xDVlFluqAnfVFOahoYmndG6qFmkZtouSW'
+}
